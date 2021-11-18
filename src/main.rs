6c3059b5bb9f96e4c6c7b0fc584ae8a3fcb644ae
Phodal Huang <h@phodal.com>
2021-11-18 21:38:07 +0800

    feat: init basic cli process

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
-pub mod model;
+// use std::fs::File;
+// use daemonize::Daemonize;
+// use livesplit_hotkey::{Hook, KeyCode};
+#[macro_use]
+extern crate log;
 
-fn main() {
-    println!("Hello, world!");
+use daemonize::Daemonize;
+use tokio_core::reactor::Core;
+
+pub mod model;
+mod main_loop;
+mod setup;
+
+
+fn main() -> Result<(), ()> {
+    env_logger::init();
+    info!("starting up");
+
+    let daemonize = Daemonize::new()
+        .pid_file("/tmp/quake.pid");
+
+    match daemonize.start() {
+        Ok(_) => println!("Success, daemonized"),
+        Err(e) => eprintln!("Error, {}", e),
+    }
+
+    let mut core = Core::new().unwrap();
+    let handle = core.handle();
+
+    let initial_state = setup::initial_state(handle);
+    core.run(initial_state).unwrap();
+
+    Ok(())
 }
