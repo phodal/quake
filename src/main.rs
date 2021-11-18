fa4a2eafa007aac80a3870a91077a0f13c2448c0
Phodal Huang <h@phodal.com>
2021-11-18 22:05:07 +0800

    poc: try use daemonize

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-// use std::fs::File;
-// use daemonize::Daemonize;
-// use livesplit_hotkey::{Hook, KeyCode};
 #[macro_use]
 extern crate log;
+
+use std::fs::File;
 
 use daemonize::Daemonize;
 use tokio_core::reactor::Core;
@@ -16,8 +15,22 @@ fn main() -> Result<(), ()> {
     env_logger::init();
     info!("starting up");
 
+    let stdout = File::create("/tmp/daemon.out").unwrap();
+    let stderr = File::create("/tmp/daemon.err").unwrap();
+
     let daemonize = Daemonize::new()
-        .pid_file("/tmp/quake.pid");
+        .pid_file("/tmp/quake.pid")
+        .chown_pid_file(true)      // is optional, see `Daemonize` documentation
+        .working_directory("/tmp") // for default behaviour.
+        .user("nobody")
+        .group("daemon") // Group name
+        .group(2)        // or group id.
+        .umask(0o777)    // Set umask, `0o027` by default.
+        .stdout(stdout)  // Redirect stdout to `/tmp/daemon.out`.
+        .stderr(stderr)  // Redirect stderr to `/tmp/daemon.err`.
+        .exit_action(|| println!("Executed before master process exits"))
+        .privileged_action(|| "Executed before drop privileges");
+
 
     match daemonize.start() {
         Ok(_) => println!("Success, daemonized"),
