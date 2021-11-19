d0cc8af88af44b7a9ac414a2379bc22c4d7ad344
Phodal Huang <h@phodal.com>
2021-11-19 22:50:02 +0800

    feat: add first search query

--- quake_core/src/concept_parser.rs
+++ quake_core/src/concept_parser.rs
@@ -1,15 +1,39 @@
+use regex::Regex;
+
+#[derive(Debug)]
 pub struct ConceptExpr {
     pub object: String,
     pub action: String,
     pub text: String,
 
     // todo: thinking in tag & at
-    pub tag: String,
-    pub at: String,
+    // pub tag: String,
+    // pub at: String,
+}
+
+impl Default for ConceptExpr {
+    fn default() -> Self {
+        ConceptExpr {
+            object: "".to_string(),
+            action: "".to_string(),
+            text: "".to_string()
+        }
+    }
 }
 
 impl ConceptExpr {
-    pub fn from(text: &str) {}
+    pub fn from(text: &str) -> ConceptExpr {
+        let regex = Regex::new("(?P<object>[a-z]+).(?P<action>[a-z]+):(?P<text>.*)").unwrap();
+
+        let mut expr = ConceptExpr::default();
+        if let Some(captures) = regex.captures(text) {
+            expr.action = String::from(&captures["action"]);
+            expr.object = String::from(&captures["object"]);
+            expr.text = String::from(&captures["text"]);
+        }
+
+        expr
+    }
 }
 
 
@@ -19,6 +43,9 @@ mod tests {
 
     #[test]
     fn should_parse_expression() {
-        ConceptExpr::from("todo.add: 添加 todo 的支持 #todo #文章")
+        let expr = ConceptExpr::from("todo.add:添加 todo 的支持");
+        assert_eq!(expr.object, "todo");
+        assert_eq!(expr.action, "add");
+        assert_eq!(expr.text, "添加 todo 的支持");
     }
 }
