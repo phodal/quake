25f58ccf7600fc7455cb953db571452f042d316d
Phodal Huang <h@phodal.com>
2021-11-21 20:33:48 +0800

    feat: try in tui

--- quake_tui/src/main.rs
+++ quake_tui/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use tui::Terminal;
+use tui::backend::CrosstermBackend;
+
+fn main() -> Result<(), io::Error> {
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    Ok(())
 }
