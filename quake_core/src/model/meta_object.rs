03e15dc6db5b7a12cb511c680b4f5ff0add6c529
Phodal Huang <h@phodal.com>
2021-11-21 21:50:17 +0800

    feat: add test for custom type

--- quake_core/src/model/meta_object.rs
+++ quake_core/src/model/meta_object.rs
@@ -1,16 +1,19 @@
 use crate::model::{Author, EntryDate};
 
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub enum MetaField {
     Text(String),
     Title(String),
-    Tagged(String),
+    Tagged(Vec<String>),
     Author(Author),
     Searchable(String),
     EntryDate(EntryDate),
     /// custom filter types
     Filterable(Vec<String>),
+    Unknown(String)
 }
 
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub enum MetaType {
     Summary,
     Note,
--- quake_core/src/model/mod.rs
+++ quake_core/src/model/mod.rs
@@ -1,21 +1,112 @@
-use std::time::SystemTime;
+use std::collections::HashMap;
+use std::time::Duration;
+
+use crate::model::meta_object::MetaField;
 
 pub mod book_review;
 pub mod meta_object;
 pub mod meta_action;
 pub mod meta_config;
 
-#[allow(dead_code)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct EntryDate {
-    created: SystemTime,
-    updated: SystemTime,
-    due_date: SystemTime,
-    resolution_date: SystemTime,
+    created: Duration,
+    updated: Duration,
+    due_date: Duration,
+    resolution_date: Duration,
 }
 
-#[allow(dead_code)]
+impl Default for EntryDate {
+    fn default() -> Self {
+        EntryDate {
+            created: Default::default(),
+            updated: Default::default(),
+            due_date: Default::default(),
+            resolution_date: Default::default(),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct Author {
     name: String,
     email: String,
 }
 
+impl Default for Author {
+    fn default() -> Self {
+        Author {
+            name: "".to_string(),
+            email: "".to_string(),
+        }
+    }
+}
+
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CustomType {
+    pub keys: Vec<String>,
+    pub fields: HashMap<String, MetaField>,
+}
+
+impl CustomType {
+    pub fn from(map: HashMap<String, String>) -> CustomType {
+        let mut keys = vec![];
+        let mut fields = HashMap::new();
+        for (key, value) in map {
+            keys.push(key.clone());
+            fields.insert(key, Self::parse_field_type(value));
+        }
+
+        CustomType {
+            keys,
+            fields,
+        }
+    }
+
+    fn parse_field_type(value: String) -> MetaField {
+        let field = match value.as_str() {
+            "Text" => {
+                MetaField::Text(value)
+            }
+            "Title" => {
+                MetaField::Title(value)
+            }
+            "Tagged" => {
+                // todo
+                let tags = vec![];
+                MetaField::Tagged(tags)
+            }
+            "Author" => {
+                let author = Author::default();
+                MetaField::Author(author)
+            }
+            "EntryDate" => {
+                let date = EntryDate::default();
+                MetaField::EntryDate(date)
+            }
+            _ => {
+                MetaField::Unknown(value)
+            }
+        };
+        field
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+    use crate::model::CustomType;
+    use crate::model::meta_object::MetaField;
+
+    #[test]
+    fn custom_type() {
+        let mut map = HashMap::new();
+        map.insert("title".to_string(), "Title".to_string());
+
+        let custom_type = CustomType::from(map);
+
+        let option = custom_type.fields.get("title").unwrap();
+        assert_eq!(&MetaField::Title(String::from("Title")), option)
+    }
+}
