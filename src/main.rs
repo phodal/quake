7eb847d82fec89a6e1880643c6b9da0c1cc05d24
Phodal Huang <h@phodal.com>
2021-11-22 23:19:46 +0800

    refactor: extract todo logic

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@ use std::fs::File;
 use std::path::PathBuf;
 
 use clap::Parser;
-use custom_entry::CustomEntry;
 
+use custom_entry::CustomEntry;
 use quake_core::concept_parser::ConceptExpr;
 use quake_core::quake_config::QuakeConfig;
+
 use crate::custom_entry::CustomEntries;
 use crate::entry_info::EntryInfo;
 use crate::slug_helper::slugify;
@@ -23,11 +24,8 @@ pub mod slug_helper;
 struct Opts {
     #[clap(short, long, default_value = ".quake.yaml")]
     config: String,
-
     #[clap(short, long)]
     input: String,
-
-    /// custom editor
     #[clap(short, long, default_value = "")]
     editor: String,
 }
@@ -69,18 +67,24 @@ fn create_action(expr: ConceptExpr, conf: QuakeConfig) {
     let mut entry_info = process_entry_info(&entry_info_path);
 
     let _ = fs::create_dir(&obj_dir);
+    let entry_file_path = obj_dir.join(format!("{:0>4}-{:}.md", entry_info.index + 1, slugify(&expr.text)));
 
     if expr.object.eq("todo") {
-        let entry_file_path = obj_dir.join(format!("{:0>4}-{:}.md", entry_info.index + 1, slugify(&expr.text)));
-
+        // todo: filter todo
         let entry = &entries_from_yaml(entries_str).entries[0];
-        if expr.action == "add" {
-            File::create(&entry_file_path).expect("Unable to create file");
-            fs::write(&entry_file_path, entry.front_matter(expr.text)).expect("cannot write to file");
 
-            entry_info.inc();
-            let result = serde_yaml::to_string(&entry_info).expect("cannot convert to yaml");
-            fs::write(&entry_info_path, result).expect("cannot write to file");
+        match expr.action.as_str() {
+            "add" => {
+                File::create(&entry_file_path).expect("Unable to create file");
+                fs::write(&entry_file_path, entry.front_matter(expr.text)).expect("cannot write to file");
+
+                entry_info.inc();
+                let result = serde_yaml::to_string(&entry_info).expect("cannot convert to yaml");
+                fs::write(&entry_info_path, result).expect("cannot write to file");
+            }
+            _ => {
+                // do_something()
+            }
         }
 
         let file_path = format!("{:}", entry_file_path.display());
@@ -114,6 +118,7 @@ fn entries_from_yaml(text: String) -> CustomEntries {
 #[cfg(test)]
 mod tests {
     use quake_core::model::meta_object::MetaField;
+
     use crate::CustomEntry;
 
     fn custom_entry_from_yaml() -> Vec<CustomEntry> {
