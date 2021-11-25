c70ea8a1b4117af4a6fa074ca3353ca81054ee8f
Phodal Huang <h@phodal.com>
2021-11-25 20:19:49 +0800

    docs: skip for git log

--- src/helper/git_cmd.rs
+++ src/helper/git_cmd.rs
@@ -75,6 +75,7 @@ mod tests {
     use crate::helper::git_cmd::{last_modify, parse_changes};
 
     #[test]
+    #[skip]
     fn last_message() {
         let last = last_modify(None, PathBuf::from("README.md"));
         let changes = parse_changes(last);
