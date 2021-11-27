042e96220bedbbeca00487538b45bc29dabdca1b
Phodal Huang <h@phodal.com>
2021-11-27 17:29:18 +0800

    feat: try jieba rs

--- quake_analysis/src/main.rs
+++ quake_analysis/src/main.rs
@@ -1,7 +1,37 @@
+use std::collections::HashMap;
+use std::fs;
+use std::path::PathBuf;
+
 use jieba_rs::Jieba;
+use walkdir::{DirEntry, WalkDir};
+
+fn is_markdown(entry: &DirEntry) -> bool {
+    entry.file_name()
+        .to_str()
+        .map(|s| s.ends_with(".md"))
+        .unwrap_or(false)
+}
 
 fn main() {
+    count_by_path(PathBuf::from("_fixtures").join("phodal_com"));
+}
+
+fn count_by_path(path: PathBuf) {
+    let mut count: HashMap<String, usize> = HashMap::new();
     let jieba = Jieba::new();
-    let words = jieba.cut("我们中出了一个叛徒", false);
-    assert_eq!(words, vec!["我们", "中", "出", "了", "一个", "叛徒"]);
+    for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
+        if is_markdown(&entry) {
+            let content = fs::read_to_string(entry.path()).unwrap();
+            let x = content.as_str();
+            let results = jieba.cut(x, true);
+            for result in results {
+                *count.entry(String::from(result)).or_insert(0) += 1;
+            }
+        }
+    }
+
+    let mut count_vec: Vec<_> = count.iter().collect();
+    count_vec.sort_by(|a, b| a.1.cmp(b.1));
+
+    println!("{:?}", count_vec);
 }
