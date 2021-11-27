475a7b93a97e0df124ded84328b4e70c83a4aa4a
Phodal Huang <h@phodal.com>
2021-11-27 17:36:50 +0800

    refactor(analysis): change to map

--- quake_analysis/src/main.rs
+++ quake_analysis/src/main.rs
@@ -24,14 +24,18 @@ fn count_by_path(path: PathBuf) {
             let content = fs::read_to_string(entry.path()).unwrap();
             let x = content.as_str();
             let results = jieba.cut(x, true);
+            let mut one_count: HashMap<String, usize> = HashMap::new();
             for result in results {
+                *one_count.entry(String::from(result)).or_insert(0) += 1;
                 *count.entry(String::from(result)).or_insert(0) += 1;
             }
+
+            let mut count_vec: Vec<_> = one_count.iter().collect();
+            count_vec.sort_by(|a, b| a.1.cmp(b.1));
+            println!("{:?}", count_vec);
         }
     }
 
-    let mut count_vec: Vec<_> = count.iter().collect();
-    count_vec.sort_by(|a, b| a.1.cmp(b.1));
-
-    println!("{:?}", count_vec);
+    // let mut count_vec: Vec<_> = count.iter().collect();
+    // count_vec.sort_by(|a, b| a.1.cmp(b.1));
 }
