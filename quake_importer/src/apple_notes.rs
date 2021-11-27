f0789821dd05a8bf9ee64242927797b28d1eb625
Phodal Huang <h@phodal.com>
2021-11-27 11:47:25 +0800

    refactor: clean deps

--- quake_importer/src/apple_notes.rs
+++ quake_importer/src/apple_notes.rs
@@ -1,34 +1,18 @@
 use std::error::Error;
 use std::fs;
-use std::io::Read;
 use std::path::PathBuf;
 
-use flate2::read::GzDecoder;
 use rusqlite::Connection;
 use rusqlite::types::ValueRef;
 
 /// refs: https://www.swiftforensics.com/2018/02/reading-notes-database-on-macos.html
-pub fn dump_apple_notes() {
-    let path = PathBuf::from("_fixtures").join("phodal.com");
-    let db_name = "../NoteStore.sqlite";
+pub fn dump_apple_notes(db_path: &str) {
+    let path = PathBuf::from("_fixtures").join("notes");
     let sql = "
-SELECT n.Z_PK, n.ZNOTE as note_id, n.ZDATA as data,
-       c3.ZFILESIZE,
-       c4.ZFILENAME, c4.ZIDENTIFIER as att_uuid,
-       c1.ZTITLE1 as title, c1.ZSNIPPET as snippet, c1.ZIDENTIFIER as noteID,
-       c1.ZCREATIONDATE1 as created, c1.ZLASTVIEWEDMODIFICATIONDATE, c1.ZMODIFICATIONDATE1 as modified,
-       c2.ZACCOUNT3, c2.ZTITLE2 as folderName, c2.ZIDENTIFIER as folderID,
-       c5.ZNAME as acc_name, c5.ZIDENTIFIER as acc_identifier, c5.ZACCOUNTTYPE
-FROM ZICNOTEDATA as n
-         LEFT JOIN ZICCLOUDSYNCINGOBJECT as c1 ON c1.ZNOTEDATA = n.Z_PK
-         LEFT JOIN ZICCLOUDSYNCINGOBJECT as c2 ON c2.Z_PK = c1.ZFOLDER
-         LEFT JOIN ZICCLOUDSYNCINGOBJECT as c3 ON c3.ZNOTE= n.ZNOTE
-         LEFT JOIN ZICCLOUDSYNCINGOBJECT as c4 ON c4.ZATTACHMENT1= c3.Z_PK
-         LEFT JOIN ZICCLOUDSYNCINGOBJECT as c5 ON c5.Z_PK = c1.ZACCOUNT2
-ORDER BY note_id
+SELECT Title, Snippet, Folder, LastModified, Data as content, User from Notes
 ";
 
-    match export_apple_notes(db_name, sql, path) {
+    match export_apple_notes(db_path, sql, path) {
         Ok(_) => {}
         Err(err) => {
             println!("{:?}", err);
@@ -54,25 +38,12 @@ fn export_apple_notes(db_name: &str, sql: &str, path: PathBuf) -> Result<(), Box
                 ValueRef::Real(real) => { real.to_string() }
                 ValueRef::Text(text) => { std::str::from_utf8(text).unwrap().to_string() }
                 ValueRef::Blob(blob) => {
-                    if blob.len() <= 0 {
-                        "".to_string()
-                    } else {
-                        let demo = format!("{:?}", blob);
-                        let mut d = GzDecoder::new(blob);
-                        let mut s = String::new();
-                        match d.read_to_string(&mut s) {
-                            Ok(so) => {}
-                            Err(err) => {
-                                println!("{:?}", err);
-                            }
-                        }
-                        s
-                    }
+                    std::str::from_utf8(blob).unwrap().to_string()
                 }
             };
 
             let name = name.to_string();
-            if name == "data" {
+            if name == "content" {
                 println!("{:?}: {:?}", name, value);
             }
         }
@@ -87,6 +58,7 @@ mod tests {
 
     #[test]
     fn dump_notes() {
-        let _ = dump_apple_notes();
+        let db_path = "../dbs/mac_apt.db";
+        let _ = dump_apple_notes(db_path);
     }
 }
