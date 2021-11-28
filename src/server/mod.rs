81f2887be76c110a52a8e5c9c5aa06460b0a6f69
Phodal Huang <h@phodal.com>
2021-11-28 18:00:46 +0800

    refactor: make static server

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,22 +1,12 @@
 use std::path::PathBuf;
+
 use rocket::Error;
+use rocket::fs::{FileServer, relative};
+
 use crate::action::entry_sets::Entrysets;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
-
-#[get("/entry/<entry_type>")]
+#[get("/<entry_type>")]
 fn entry(entry_type: &str) -> String {
-    let path = PathBuf::from("_fixtures").join(entry_type);
-    let result = Entrysets::jsonify(&path);
-    result.unwrap()
-}
-
-#[allow(dead_code)]
-#[post("/entry/<entry_type>")]
-fn entry_create(entry_type: &str) -> String {
     let path = PathBuf::from("_fixtures").join(entry_type);
     let result = Entrysets::jsonify(&path);
     result.unwrap()
@@ -25,7 +15,8 @@ fn entry_create(entry_type: &str) -> String {
 #[rocket::main]
 pub async fn start_server() -> Result<(), Error> {
     rocket::build()
-        .mount("/", routes![index, entry])
+        .mount("/", FileServer::from(relative!("quake_webapp")))
+        .mount("/entry", routes![entry])
         .launch()
         .await
 }
