bf28867a39407d49e93043cb363e38a41c6c23d7
Phodal Huang <h@phodal.com>
2021-11-29 13:28:51 +0800

    feat: try server config

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,4 +1,7 @@
-use rocket::Error;
+use rocket::{Config, Error};
+use rocket::fairing::AdHoc;
+use rocket::figment::{Figment, Profile};
+use rocket::figment::providers::{Env, Format, Serialized, Toml};
 use rocket::fs::{FileServer, relative};
 
 #[allow(unused_imports)]
@@ -14,12 +17,25 @@ pub struct ApiError {
     pub msg: String,
 }
 
+#[derive(Debug, Deserialize, Serialize)]
+pub struct QuakeServerConfig {
+
+}
+
 #[rocket::main]
 pub async fn start_server() -> Result<(), Error> {
-    rocket::build()
+    let figment = Figment::from(rocket::Config::default())
+        .merge(Serialized::defaults(Config::default()))
+        .merge(Toml::file("App.toml").nested())
+        .merge(Env::prefixed("APP_").global())
+        .select(Profile::from_env_or("APP_PROFILE", "default"));
+
+
+    rocket::custom(figment)
         .mount("/", FileServer::from(relative!("quake_webapp")))
         .mount("/entry", routes![entry_api::entry])
         .mount("/action", routes![action_api::parse_query, action_api::suggest])
+        .attach(AdHoc::config::<QuakeServerConfig>())
         .launch()
         .await
 }
