7de66f64294dc841cb6e874a15ac76fe01a330aa
Phodal Huang <h@phodal.com>
2021-11-29 12:58:35 +0800

    feat(webapp): make action & search split

--- quake_webapp/README.md
+++ quake_webapp/README.md
@@ -9,10 +9,14 @@ Todo:
 - [ ] Editor: https://github.com/KillerCodeMonkey/stencil-quill
   - [ ] Notion Like: [https://github.com/konstantinmuenster/notion-clone](https://github.com/konstantinmuenster/notion-clone)
   - [ ] [react-tapable-editor](https://github.com/ryuever/react-tapable-editor) with draft.js
-  - 
-- [ ] SearchBox with Action
-  - [ ] Quake Action 
-    - [ ] edit to editor view
+- [x] SearchBox with Action
+  - [x] Quake Action
+- [ ] Stencil.js Router
+  - [ ] [https://github.com/ionic-team/stencil-router](https://github.com/ionic-team/stencil-router)
+  - [ ] edit to editor view
+- [ ] Web Components
+  - [https://reactjs.org/docs/web-components.html](https://reactjs.org/docs/web-components.html)
+  - [https://levelup.gitconnected.com/convert-existing-react-components-intowebcomponents-2b33b842ff9a](https://levelup.gitconnected.com/convert-existing-react-components-intowebcomponents-2b33b842ff9a) 
 - [ ] Dashboard
 - [ ] ChartView
 - [ ] Ledge Render
--- quake_webapp/dashboard/src/components/quake-dashboard/quake-dashboard.tsx
+++ quake_webapp/dashboard/src/components/quake-dashboard/quake-dashboard.tsx
@@ -1,7 +1,12 @@
-import {Component, Prop, h, Element, State} from '@stencil/core';
+import {Component, Element, h, Prop, State} from '@stencil/core';
 import {MeiliSearch} from "meilisearch";
+import dayjs from "dayjs";
+
+// only for: IDEA jump
 // @ts-ignore
-import { IonSearchbar } from "@ionic/core";
+import {IonSearchbar} from "@ionic/core";
+import axios from "axios";
+
 
 @Component({
   tag: 'quake-dashboard',
@@ -16,26 +21,34 @@ export class QuakeDashboard {
   @State() items: Array<object> = [];
   @State() isAction = false;
 
+  @State() query: String = "";
+  @State() actionType: String = "";
+
   client = new MeiliSearch({
     host: 'http://127.0.0.1:7700'
   })
 
   handleInput(event) {
+    console.log(event);
     const that = this;
-    const query = event.target.value;
-    if(query.length == 0) {
+    this.query = event.target.value;
+    if (this.query.length == 0) {
+      that.items = [];
+      this.actionType = '';
       return;
     }
 
-    if (query.startsWith(":")) {
-      this.isAction = true;
-
-      // suggest entry type for action
-
-      // suggest entry action for actions
+    if (this.query.startsWith(".")) {
+      this.actionType = 'Action'
       return;
     }
 
+    this.actionType = 'Search';
+    this.createSearch(that);
+  }
+
+  private createSearch(that: this) {
+    let query = that.query.substr(0, 1);
     const index = this.client.index('phodal_com')
     requestAnimationFrame(() => {
       index.search(query).then((result) => {
@@ -44,27 +57,60 @@ export class QuakeDashboard {
     });
   }
 
+  formatDate(str) {
+    return dayjs(str).format('YYYY-MM-DD HH:mm:ss');
+  }
+
   clearInput(_event) {
     this.items = [];
+  }
+
+  private async handleSubmit(e) {
+    e.preventDefault()
+
+    axios.get('http://127.0.0.1:8000/action/query/', {
+      params: {
+        input: this.query.substring(1,)
+      }
+    }).then(function (response) {
+      console.log(response.data);
+    }).catch(function (error) {
+      console.log(error);
+    });
   }
 
   render() {
     return <ion-app>
       <ion-header translucent>
         <ion-toolbar>
-          <ion-title>Quake Action</ion-title>
+          <ion-title>Quake</ion-title>
         </ion-toolbar>
         <ion-toolbar>
-          <ion-searchbar ref={(el) => this.ionInputElement = el}
-                         onIonCancel={this.clearInput.bind(this)}
-                         onIonClear={this.clearInput.bind(this)}
-                         onIonChange={this.handleInput.bind(this)}></ion-searchbar>
+          <ion-item>
+            {this.actionType.length > 0 ? <ion-chip>
+              <ion-label>{this.actionType}</ion-label>
+            </ion-chip> : null}
+            <form onSubmit={(e) => this.handleSubmit(e)}>
+              <ion-input
+                placeholder="`.todo:add` for create `todo`"
+                autofocus={true}
+                ref={(el) => this.ionInputElement = el}
+                onIonInput={this.handleInput.bind(this)}
+              />
+              <input type="submit" value="Submit" id="submit-button"/>
+            </form>
+            {/*{ this.actionType.length > 0 ? <ion-chip><ion-label>{this.actionType}</ion-label></ion-chip> : null }*/}
+          </ion-item>
         </ion-toolbar>
       </ion-header>
       <ion-content fullscreen>
         <ion-list>
           {this.items.length > 0
-            ? this.items.map((item: any) => <ion-item>{item.title}</ion-item>)
+            ? this.items.map((item: any) =>
+              <ion-item>
+                <ion-badge slot="start">{this.formatDate(item.created_date)}</ion-badge>
+                {item.title}
+              </ion-item>)
             : <div></div>
           }
         </ion-list>
