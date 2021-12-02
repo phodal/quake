b2dc146aa1fb503420accad6fe4d7dd42cc2ceee
Phodal Huang <h@phodal.com>
2021-12-02 08:13:15 +0800

    fix: fix feed url refactor issue

--- src/helper/meili_exec.rs
+++ src/helper/meili_exec.rs
@@ -2,7 +2,7 @@ use std::error::Error;
 use std::process::Command;
 
 pub fn feed_command(path_name: &String) -> Result<(), Box<dyn Error>> {
-    let url = format!("{:?}", path_name);
+    let url = format!("http://127.0.0.1:7700/indexes/{:}/documents", path_name);
     Command::new("/bin/sh")
         .arg("-c")
         .arg(format!("curl -i -X POST '{:}' \
