2eabb2b157519c73aa473b45ce50cd891bc7029e
Phodal Huang <h@phodal.com>
2021-12-02 21:17:49 +0800

    feat: make copy works

--- quake_webapp/build.js
+++ quake_webapp/build.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const cheerio = require("cheerio");
-const http = require('http');
+const fse = require('fs-extra');
 
 let content = fs.readFileSync('index.html');
 let text = content.toString();
@@ -14,18 +14,38 @@ if (!fs.existsSync("dist/js")){
   fs.mkdirSync("dist/js");
 }
 
+if (!fs.existsSync("dist/js/dashboard")){
+  fs.mkdirSync("dist/js/dashboard");
+}
+
+const srcDir = `./dashboard/dist/dashboard/`;
+const destDir = `./dist/js/dashboard/`;
+
+// To copy a folder or file
+fse.copySync(srcDir, destDir, { overwrite: true }, function (err) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("success!");
+  }
+});
+
 $('body > script').map((i, script) => {
   let attribs = script.attribs;
   if (!attribs.src.startsWith('http')) {
     let split = attribs.src.split("/");
     let file_name = split[split.length - 1];
 
-    text = text.replace(attribs.src, `js/${file_name}`);
+    if (attribs.type !== 'module') {
+      text = text.replace(attribs.src, `js/${file_name}`);
 
-    fs.copyFile(attribs.src, `dist/js/${file_name}`, (err) => {
-      if (err) throw err;
-      console.log('source.txt was copied to destination.txt');
-    });
+      fs.copyFile(attribs.src, `dist/js/${file_name}`, (err) => {
+        if (err) throw err;
+      });
+    } else {
+      let dir = file_name.split(".")[0];
+      text = text.replace(attribs.src, `js/${dir}/${file_name}`);
+    }
   }
 })
 
