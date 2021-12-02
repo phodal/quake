1e78b2ba98e4e2296096fcdd11a186abcc4b3cf7
Phodal Huang <h@phodal.com>
2021-12-02 21:05:52 +0800

    build: try to write copy scripts

--- quake_webapp/build.js
+++ quake_webapp/build.js
@@ -3,8 +3,30 @@ const cheerio = require("cheerio");
 const http = require('http');
 
 let content = fs.readFileSync('index.html');
-const $ = cheerio.load(content.toString());
+let text = content.toString();
+const $ = cheerio.load(text);
+
+if (!fs.existsSync("dist")){
+  fs.mkdirSync("dist");
+}
+
+if (!fs.existsSync("dist/js")){
+  fs.mkdirSync("dist/js");
+}
 
 $('body > script').map((i, script) => {
-  console.log(script.attribs)
+  let attribs = script.attribs;
+  if (!attribs.src.startsWith('http')) {
+    let split = attribs.src.split("/");
+    let file_name = split[split.length - 1];
+
+    text = text.replace(attribs.src, `js/${file_name}`);
+
+    fs.copyFile(attribs.src, `dist/js/${file_name}`, (err) => {
+      if (err) throw err;
+      console.log('source.txt was copied to destination.txt');
+    });
+  }
 })
+
+fs.writeFileSync('dist/index.html', text);
--- quake_webapp/dashboard/stencil.config.ts
+++ quake_webapp/dashboard/stencil.config.ts
@@ -2,6 +2,7 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'dashboard',
+  buildEs5: true,
   outputTargets: [
     {
       type: 'dist',
