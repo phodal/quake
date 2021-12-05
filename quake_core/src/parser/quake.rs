e6a1528d61da397168e2b08072af7a996dedb57e
Phodal Huang <h@phodal.com>
2021-12-05 09:27:25 +0800

    feat: make transflow

--- quake_core/src/parser/quake.rs
+++ quake_core/src/parser/quake.rs
@@ -1,13 +1,13 @@
 use std::error::Error;
 
-use crate::parser::ast::SourceUnitPart;
+use crate::parser::ast::{SourceUnitPart, TransflowDecl, TransflowEnum};
 use crate::parser::errors::QuakeParserError;
 use crate::parser::parser::parse;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct QuakeIt {
     pub actions: Vec<QuakeAction>,
-    pub transflows: Vec<Transflow>,
+    pub transflows: Vec<QuakeTransflow>,
 }
 
 impl Default for QuakeIt {
@@ -19,15 +19,43 @@ impl Default for QuakeIt {
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Transflow {
+impl QuakeTransflow {
+    pub fn from_text(text: &str) -> Result<QuakeTransflow, Box<dyn Error>> {
+        let it = quake(text)?;
+        if it.transflows.is_empty() {
+            return Err(Box::new(QuakeParserError::new("not match transflows")));
+        }
+
+        Ok(it.transflows[0].clone())
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct QuakeTransflow {
     pub routes: Vec<Route>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+impl Default for QuakeTransflow {
+    fn default() -> Self {
+        QuakeTransflow { routes: vec![] }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Route {
     pub from: Vec<String>,
     pub to: String,
+    pub is_component: bool,
+}
+
+impl Default for Route {
+    fn default() -> Self {
+        Route {
+            from: vec![],
+            to: "".to_string(),
+            is_component: false,
+        }
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -84,16 +112,52 @@ pub fn quake(text: &str) -> Result<QuakeIt, Box<dyn Error>> {
 
                 quakes.actions.push(action);
             }
-            SourceUnitPart::Transflow(_transflow) => {}
+            SourceUnitPart::Transflow(decl) => {
+                let transflow = build_transflow(decl);
+                quakes.transflows.push(transflow);
+            }
         }
     }
 
     Ok(quakes)
 }
 
+fn build_transflow(decl: TransflowDecl) -> QuakeTransflow {
+    let mut transflow = QuakeTransflow::default();
+    transflow.routes = decl
+        .flows
+        .iter()
+        .map(|flow_decl| {
+            let mut route = Route::default();
+            match flow_decl {
+                TransflowEnum::Midway(way) => {
+                    route.to = way.end.clone();
+                    route.from = way
+                        .from
+                        .iter()
+                        .map(|param| param.value.clone())
+                        .collect::<Vec<String>>()
+                }
+                TransflowEnum::Endway(way) => {
+                    route.to = way.component.clone();
+                    route.is_component = true;
+                    route.from = way
+                        .from
+                        .iter()
+                        .map(|param| param.value.clone())
+                        .collect::<Vec<String>>()
+                }
+            }
+            route
+        })
+        .collect::<Vec<Route>>();
+    transflow
+}
+
 #[cfg(test)]
 mod tests {
     use crate::parser::quake::QuakeAction;
+    use crate::quake::QuakeTransflow;
 
     #[test]
     fn should_parse_expression() {
@@ -141,7 +205,9 @@ mod tests {
     #[test]
     fn should_create_transflow() {
         let define = "define { from('todo','blog').to(<quake-calendar>); }";
-        // let expr = QuakeAction::action_from_text(define).unwrap();
-        // println!("{:?}", expr);
+        let expr = QuakeTransflow::from_text(define).unwrap();
+        assert_eq!(1, expr.routes.len());
+        assert_eq!(true, expr.routes[0].is_component);
+        assert_eq!("quake-calendar", expr.routes[0].to);
     }
 }
