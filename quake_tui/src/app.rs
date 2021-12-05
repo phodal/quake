ca64dbc8e7a392b82f41691daedbb8e560d3b8f6
Elliot <hack00mind@gmail.com>
2021-12-05 21:19:37 +0800

    feat(tui): simple command mode

--- quake_tui/src/app.rs
+++ quake_tui/src/app.rs
@@ -1,39 +1,31 @@
-use std::path::PathBuf;
-use std::{fs, io};
-
 pub struct App {
-    pub entry_dirs: Vec<PathBuf>,
+    pub mode: Mode,
+    pub command: String,
+    pub main_widget: MainWidget,
 }
 
 impl App {
     pub fn new() -> App {
         App::default()
     }
-
-    pub fn on_tick(&mut self) {
-        if let Ok(dirs) = list_entries_dirs() {
-            self.entry_dirs = dirs;
-        }
-    }
 }
 
 impl Default for App {
     fn default() -> Self {
-        App { entry_dirs: vec![] }
+        App {
+            mode: Mode::Normal,
+            command: "".to_string(),
+            main_widget: MainWidget::Home,
+        }
     }
 }
 
-fn list_entries_dirs() -> io::Result<Vec<PathBuf>> {
-    let mut entries = fs::read_dir(".")?
-        .map(|res| res.map(|e| e.path()))
-        .filter(|path| path.as_ref().map(is_entries_dir).unwrap_or(false))
-        .collect::<Result<Vec<_>, io::Error>>()?;
-
-    entries.sort();
-
-    Ok(entries)
+pub enum Mode {
+    Command,
+    Normal,
 }
 
-fn is_entries_dir(path: &PathBuf) -> bool {
-    path.is_dir() && path.join("entries.csv").exists()
+pub enum MainWidget {
+    Home,
+    Dirs,
 }
