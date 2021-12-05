d895a7a7842f55fe73f7436f4e5412aabc74a653
Phodal Huang <h@phodal.com>
2021-12-05 21:51:16 +0800

    fix: fix typo

--- quake_core/src/entry/mod.rs
+++ quake_core/src/entry/mod.rs
@@ -1,13 +1,12 @@
-pub mod entry_define;
-pub mod entry_file;
-pub mod entry_node_info;
-// pub mod entry_entity;
-pub mod entry_defines;
-pub mod entry_paths;
-pub mod front_matter;
-pub mod slug;
-
 pub use entry_define::EntryDefine;
 pub use entry_defines::EntryDefines;
 pub use entry_node_info::EntryNodeInfo;
 pub use front_matter::FrontMatter;
+
+pub mod entry_define;
+pub mod entry_defines;
+pub mod entry_file;
+pub mod entry_node_info;
+pub mod entry_paths;
+pub mod front_matter;
+pub mod slug;
