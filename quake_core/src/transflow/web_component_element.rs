5b41f93b9e432b467ff2157f89c2c68c6dd78191
Phodal Huang <h@phodal.com>
2021-12-07 08:24:42 +0800

    feat: add init wce logic

--- quake_core/src/transflow/web_component_element.rs
+++ quake_core/src/transflow/web_component_element.rs
@@ -1,7 +1,12 @@
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
 pub struct WebComponentElement {
+    /// element id, such as `quake-dashboard`
     pub id: String,
+    /// element's input attributes, such
+    /// data in `<quake-dashboard data=""></quake-dashboard>`
     pub attributes: Vec<Attribute>,
+    /// output events
+    pub events: Vec<EventListener>,
 }
 
 impl Default for WebComponentElement {
@@ -9,14 +14,42 @@ impl Default for WebComponentElement {
         WebComponentElement {
             id: "".to_string(),
             attributes: vec![],
+            events: vec![],
         }
+    }
+}
+
+impl WebComponentElement {
+    pub fn from_js(
+        element: &str,
+        attributes: Vec<String>,
+        events: Vec<String>,
+    ) -> WebComponentElement {
+        let mut wce = Self::default();
+        wce.id = element.to_string();
+
+        for attr in attributes {
+            wce.attributes.push(Attribute {
+                typ: None,
+                name: attr,
+            })
+        }
+
+        for event in events {
+            wce.events.push(EventListener {
+                event_name: event,
+                event_data: None,
+            })
+        }
+
+        wce
     }
 }
 
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
 pub struct Attribute {
     #[serde(rename = "type")]
-    pub typ: String,
+    pub typ: Option<String>,
     pub name: String,
 }
 
@@ -30,12 +63,13 @@ pub enum AttributeType {
     String(String),
 }
 
-pub type WcValue = Attribute;
+pub type EventValue = Attribute;
 
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
 pub struct EventListener {
     pub event_name: String,
-    pub values: Vec<WcValue>,
+    /// to get `event.detail`
+    pub event_data: Option<Vec<EventValue>>,
 }
 
 #[cfg(test)]
@@ -43,7 +77,15 @@ mod tests {
     use crate::transflow::web_component_element::WebComponentElement;
 
     #[test]
-    fn mapping_to_file() {
-        let _ele = WebComponentElement::default();
+    fn test_web_component_element_struct() {
+        let wce = WebComponentElement::from_js(
+            "quake-dashboard",
+            vec!["data".to_string()],
+            vec!["onSave".to_string()],
+        );
+
+        assert_eq!("quake-dashboard", wce.id);
+        assert_eq!(1, wce.events.len());
+        assert_eq!(1, wce.attributes.len());
     }
 }
