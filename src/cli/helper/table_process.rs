772f215e95474f23a80fc008cc463e8dcb70cb64
Phodal Huang <h@phodal.com>
2021-12-08 12:38:13 +0800

    build: set vesion to 2021

--- src/cli/helper/table_process.rs
+++ src/cli/helper/table_process.rs
@@ -7,7 +7,6 @@ pub fn csv_to_terminal_table(rdr: &mut Reader<File>) -> Table {
     let mut table = Table::new();
 
     let mut header = vec![];
-    header.push("id".to_string());
     for record in rdr.headers() {
         for str in record {
             header.push(String::from(str))
@@ -16,15 +15,12 @@ pub fn csv_to_terminal_table(rdr: &mut Reader<File>) -> Table {
 
     table.set_header(header);
 
-    let mut index = 1;
     for result in rdr.records() {
         let record = result.unwrap();
         let mut row = vec![];
-        row.push(index.to_string());
         for str in &record {
             row.push(String::from(str));
         }
-        index = index + 1;
         table.add_row(row);
     }
 
