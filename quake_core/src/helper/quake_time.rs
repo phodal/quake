04be50ec7b8f5d9437bba42e693216f13cdca6e7
Phodal Huang <h@phodal.com>
2021-12-09 21:01:35 +0800

    feat(core): add basic time support

--- quake_core/src/helper/quake_time.rs
+++ quake_core/src/helper/quake_time.rs
@@ -1,17 +1,24 @@
-use chrono::{DateTime, Local};
+use chrono::{DateTime, Local, NaiveDate, NaiveDateTime};
 use lazy_static::lazy_static;
 use regex::Regex;
 
-const FORMAT: &'static str = "%Y-%m-%d %H:%M:%S";
-const ISO8601_DATE: &'static str = "%Y-%m-%d %H:%M:%S";
+const DATETIME_ZONE_FORMAT: &'static str = "%Y-%m-%d %H:%M:%S %z";
+const DATETIME_FORMAT: &'static str = "%Y-%m-%d %H:%M:%S";
+const DATE_FORMAT: &'static str = "%Y-%m-%d";
+
 lazy_static! {
     static ref ISO8601_DATE_REGEX: Regex =
         Regex::new(r"(?P<y>\d{4})-(?P<m>\d{2})-(?P<d>\d{2})").unwrap();
+    static ref ISO8601_DATE_TIME_REGEX: Regex =
+        Regex::new(r"((?P<y>\d{4})-(?P<m>\d{2})-(?P<d>\d{2})\s\d{2}:\d{2}:\d{2})").unwrap();
+    static ref ISO8601_DATE_TIME_ZONE_REGEX: Regex =
+        Regex::new(r"((?P<y>\d{4})-(?P<m>\d{2})-(?P<d>\d{2})\s\d{2}:\d{2}:\d{2}\s\+\d{2}:\d{2})")
+            .unwrap();
 }
 
 pub fn date_now() -> String {
     let local: DateTime<Local> = Local::now();
-    local.format(FORMAT).to_string()
+    local.format(DATETIME_FORMAT).to_string()
 }
 
 /// see in: https://stackoverflow.com/questions/61179070/rust-chrono-parse-date-string-parseerrornotenough-and-parseerrortooshort
@@ -25,26 +32,36 @@ pub fn date_now() -> String {
 /// Date + Time + Timezone (other or non-standard)
 /// Date + Time
 /// Date
-pub fn text_date_to_unix() {}
+pub fn text_date_to_unix(text: &str) -> i64 {
+    let timestamp = 0;
+
+    if let Some(_caps) = ISO8601_DATE_TIME_ZONE_REGEX.captures(text) {
+        let naive_date_time = DateTime::parse_from_str(text, DATETIME_ZONE_FORMAT).unwrap();
+        return naive_date_time.timestamp();
+    };
+
+    if let Some(_caps) = ISO8601_DATE_TIME_REGEX.captures(text) {
+        let naive_date_time = NaiveDateTime::parse_from_str(text, DATETIME_FORMAT).unwrap();
+        return naive_date_time.timestamp();
+    };
+
+    if let Some(_caps) = ISO8601_DATE_REGEX.captures(text) {
+        let naive_date = NaiveDate::parse_from_str(text, DATE_FORMAT).unwrap();
+        let naive_datetime = naive_date.and_hms(0, 0, 0);
+        return naive_datetime.timestamp();
+    };
+
+    timestamp
+}
 
 #[cfg(test)]
 mod tests {
-    use crate::helper::quake_time::ISO8601_DATE_REGEX;
-    use chrono::{DateTime, NaiveDate, NaiveDateTime};
+    use crate::helper::quake_time::text_date_to_unix;
 
     #[test]
     fn time_regex_match() {
-        let time = "2021-12-09";
-        let timestamp = match ISO8601_DATE_REGEX.captures(&time) {
-            Some(_caps) => {
-                let naive_date = NaiveDate::parse_from_str(time, "%Y-%m-%d").unwrap();
-                let naive_datetime = naive_date.and_hms(0, 0, 0);
-
-                naive_datetime.timestamp()
-            }
-            _ => 0,
-        };
-
-        assert_eq!(timestamp, 1639008000);
+        assert_eq!(text_date_to_unix("2020-04-12 22:10:57 +08:00"), 1586700657);
+        assert_eq!(text_date_to_unix("2020-04-12 22:10:57"), 1586729457);
+        assert_eq!(text_date_to_unix("2021-12-09"), 1639008000);
     }
 }
