b04bc25cd30d36ff1bbea7d183aff6f7ab37b747
Phodal Huang <h@phodal.com>
2021-12-14 22:01:09 +0800

    feat(webapp): add data for graph:

--- quake_webapp/quake-render/src/components/quake-graph/quake-graph.tsx
+++ quake_webapp/quake-render/src/components/quake-graph/quake-graph.tsx
@@ -1,5 +1,5 @@
-import { Component, h, Host, Prop } from '@stencil/core';
-import * as d3 from 'd3';
+import {Component, h, Host, Prop, State} from '@stencil/core';
+import * as echarts from 'echarts';
 
 @Component({
   tag: 'quake-graph',
@@ -7,67 +7,70 @@ import * as d3 from 'd3';
   shadow: true,
 })
 export class QuakeGraph {
+  @State() myChart: any;
   element!: HTMLElement;
   @Prop() config: any = {};
-  @Prop() data: any = [{
-    children: [{
-      name: 'Hello',
-      value: 2,
-    }, {
-      name: 'World',
-      value: 2,
-    }],
-    name: 'Demo',
-    checked: true,
-  }];
-
-  private value: number = 0;
-  private holder: any;
-
-  componentDidRender() {
-    this.renderGraph(this.element);
-  }
-
-  private inputChanged = (event: Event) => {
-    this.value = Number((event.target as HTMLInputElement).value);
-    this.update(this.value);
+  @Prop() data: any = {
+    nodes: [
+      {id: 0, name: 'hello', category: 0},
+      {id: 1, name: 'world', category: 1}
+    ],
+    links: [{source: "1", target: "0"}],
+    categories: [{name: 'A'}, {name: 'B'}]
   };
 
-  private update(nRadius) {
-    this.holder.selectAll('circle')
-      .attr('r', nRadius);
+  componentDidRender() {
+    this.myChart = echarts.init(this.element);
+    this.renderGraph();
   }
 
   render() {
     return (
       <Host>
-        <p>
-          <label htmlFor='nRadius'>
-            {this.value}
-          </label>
-          <input type='range' min='1' max='150' id='nRadius' value={this.value} onChange={this.inputChanged} />
-        </p>
-        <div class='chart' ref={(el) => this.element = el as HTMLElement} />
+        <div class='chart' ref={(el) => this.element = el as HTMLElement}/>
       </Host>
     );
   }
 
-  private renderGraph(chartElement: any) {
-    const width = 600;
-    const height = 300;
+  private renderGraph() {
+    this.myChart.hideLoading();
+    this.data.nodes.forEach(function (node) {
+      node.symbolSize = 10;
+    });
+    const option: any = {
+      title: {
+        text: '',
+        subtext: 'Default layout',
+        top: 'bottom',
+        left: 'right'
+      },
+      tooltip: {},
+      legend: [
+        {
+          data: this.data.categories.map(function (a) {
+            return a.name;
+          })
+        }
+      ],
+      series: [
+        {
+          name: '',
+          type: 'graph',
+          layout: 'force',
+          data: this.data.nodes,
+          links: this.data.links,
+          categories: this.data.categories,
+          roam: true,
+          label: {
+            position: 'right'
+          },
+          force: {
+            repulsion: 100
+          }
+        }
+      ]
+    };
 
-    this.holder = d3.select(chartElement)
-      .append('svg')
-      .attr('width', width)
-      .attr('height', height);
-
-    this.holder.append('circle')
-      .attr('cx', 300)
-      .attr('cy', 150)
-      .style('fill', 'none')
-      .style('stroke', 'blue')
-      .attr('r', 120);
-
-    this.update(120);
+    this.myChart.setOption(option);
   }
 }
