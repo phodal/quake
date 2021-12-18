69d1876908701603b791fbd7b0c49e9898e4ccba
Phodal Huang <h@phodal.com>
2021-12-18 23:09:20 +0800

    feat(wasm): export action for js

--- quake_wasm/src/lib.rs
+++ quake_wasm/src/lib.rs
@@ -3,16 +3,19 @@ use wasm_bindgen::prelude::*;
 use quake_core::quake::{QuakeActionNode, QuakeTransflowNode, SimpleLayout};
 
 #[wasm_bindgen]
-pub fn parse_transflow(string: &str) {
-    QuakeTransflowNode::from_text(string).unwrap();
+pub fn parse_transflow(string: &str) -> String {
+    let flow = QuakeTransflowNode::from_text(string).unwrap();
+    serde_json::to_string(&flow).unwrap()
 }
 
 #[wasm_bindgen]
-pub fn parse_action(string: &str) {
-    QuakeActionNode::from_text(string).unwrap();
+pub fn parse_action(string: &str) -> String {
+    let node = QuakeActionNode::from_text(string).unwrap();
+    serde_json::to_string(&node).unwrap()
 }
 
 #[wasm_bindgen]
-pub fn parse_layout(string: &str) {
-    SimpleLayout::from_text(string).unwrap();
+pub fn parse_layout(string: &str) -> String {
+    let layout = SimpleLayout::from_text(string).unwrap();
+    serde_json::to_string(&layout).unwrap()
 }
