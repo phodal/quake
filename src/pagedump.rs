7ccfa469ff25225f594ddfdf3dcf26af384e85aa
Phodal Huang <h@phodal.com>
2021-12-19 14:02:28 +0800

    feat(dump): export json for defines

--- src/pagedump.rs
+++ src/pagedump.rs
@@ -1,7 +1,14 @@
+use quake_core::entry::entry_paths::EntryPaths;
+use quake_core::entry::EntryDefines;
 use quake_core::QuakeConfig;
+use std::fs;
+use std::path::PathBuf;
+
+static DUMP_PATH: &str = "pagedump";
 
 // export data for GitHub pages as demo
 pub fn page_dump(conf: QuakeConfig) {
+    fs::create_dir_all(DUMP_PATH).unwrap();
     // 1. dump entries config;
     dump_entries_define(&conf);
     // 2. dump quake information;
@@ -12,20 +19,45 @@ pub fn page_dump(conf: QuakeConfig) {
     dump_entries_data();
 }
 
-fn dump_transflow() {
-    todo!()
+fn dump_transflow() {}
+
+fn dump_layout() {}
+
+fn dump_links() {}
+
+fn dump_entries_define(conf: &QuakeConfig) {
+    let path = PathBuf::from(&conf.workspace);
+    let defines = EntryDefines::from_path(&path.join(EntryPaths::entries_define()));
+
+    let content = serde_json::to_string(&defines).unwrap();
+    let out_path = PathBuf::from(DUMP_PATH).join("defines.json");
+
+    fs::write(out_path, content).unwrap();
 }
 
-fn dump_layout() {
-    todo!()
-}
+fn dump_entries_data() {}
 
-fn dump_links() {
-    todo!()
-}
+#[cfg(test)]
+mod tests {
+    use crate::page_dump;
+    use crate::pagedump::DUMP_PATH;
+    use quake_core::QuakeConfig;
+    use std::path::PathBuf;
 
-fn dump_entries_define(_conf: &QuakeConfig) {}
+    fn config() -> QuakeConfig {
+        QuakeConfig {
+            workspace: "examples".to_string(),
+            editor: "".to_string(),
+            search_url: "http://127.0.0.1:7700".to_string(),
+            server_location: "web".to_string(),
+            port: 8000,
+        }
+    }
 
-fn dump_entries_data() {
-    todo!()
+    #[test]
+    fn should_dump_entries_define() {
+        page_dump(config());
+        let output = PathBuf::from(DUMP_PATH).join("defines.json");
+        assert!(output.exists());
+    }
 }
