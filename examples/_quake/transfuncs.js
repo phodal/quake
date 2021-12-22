54a6e887c9dbb35c72f84a32b7dc33fed74fa8b7
Phodal Huang <h@phodal.com>
2021-12-23 00:01:01 +0800

    refactor: move tranfuncs design to funcs

---
+++ examples/_quake/transfuncs.js
@@ -0,0 +1,53 @@
+function from_quake_references_to_network(data) {
+  let category_index = [];
+  let nodes = [];
+  let links = [];
+
+  for (let node of data) {
+    if (!category_index.includes(node.source_type)) {
+      category_index.push(node.source_type);
+    }
+    nodes.push({
+      id: parseInt(node.source_id),
+      name: node.source_title,
+      category: category_index.indexOf(node.source_type)
+    });
+
+    for (let ref of node.references) {
+      if (!category_index.includes(ref.entry_type)) {
+        category_index.push(ref.entry_type);
+      }
+
+      nodes.push({
+        id: parseInt(ref.entry_id),
+        name: ref.entry_title,
+        category: category_index.indexOf(ref.entry_type)
+      })
+
+      links.push({source: parseInt(node.source_id) + "", target: parseInt(ref.entry_id) + ""});
+    }
+  }
+
+  let categories = [];
+  for (let category of category_index) {
+    categories.push({name: category})
+  }
+  return {
+    nodes,
+    links,
+    categories
+  }
+}
+
+const tl_show_network = async (context, commands) => {
+  const el = document.createElement('graph-network');
+
+  let response = await fetch("/reference/quake_book");
+  let data = from_quake_references_to_network(await response.json());
+  console.log(data);
+  el.data = data;
+
+  return el;
+}
+
+Quake.flows['tl_show_network'] = tl_show_network;
