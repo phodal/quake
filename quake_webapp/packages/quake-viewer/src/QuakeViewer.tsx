b29a424f61c5156c4d490d470e008ca353174073
Phodal Huang <h@phodal.com>
2022-01-09 22:04:35 +0800

    feat(viewew): add pdf view demo

--- quake_webapp/packages/quake-viewer/src/QuakeViewer.tsx
+++ quake_webapp/packages/quake-viewer/src/QuakeViewer.tsx
@@ -1,27 +1,49 @@
 import React from 'react';
+import {Document, Page, pdfjs} from "react-pdf";
+import styled from "styled-components";
+// issues from: https://github.com/wojtekmaj/react-pdf/issues/97
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 export type Props = {
   type: any,
-  data: any,
+  file: any,
 }
 
 function QuakeViewer(props: Props) {
-  const [type] = React.useState(props.type);
-  const [data, setData] = React.useState(props.data);
+  const [type, setType] = React.useState(props.type);
+  const [file, setFile] = React.useState(props.file);
+
+  const [numPages, setNumPages] = React.useState(null);
+  const [pageNumber, setPageNumber] = React.useState(1);
 
   React.useEffect(() => {
-    setData(props.data);
+    setType(props.type);
   }, [props])
 
+  React.useEffect(() => {
+    setFile(props.file);
+  }, [props])
+
+  // @ts-ignore
+  function onLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
   return (
-    <div >
-      <h1>hello, world!</h1>
-      <h2>{data}</h2>
-      { type === "pdf" &&
-        <h2>PDF</h2>
+    <div>
+      {type === "pdf" &&
+        <StyleDocument file={file} onLoadSuccess={onLoadSuccess}>
+          <Page pageNumber={pageNumber} />
+        </StyleDocument>
       }
     </div>
   );
 }
+
+const StyleDocument = styled(Document)`
+  height: 100%;
+  width: 100%;
+  min-height: 500px;
+`;
 
 export default QuakeViewer;
