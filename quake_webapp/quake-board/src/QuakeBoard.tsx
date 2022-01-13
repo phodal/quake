c80ce48bfd9af5167b25d81ace79edb3c2dd31b2
Phodal Huang <h@phodal.com>
2022-01-13 12:32:35 +0800

    feat: add source & targe demo

--- quake_webapp/quake-board/src/QuakeBoard.tsx
+++ quake_webapp/quake-board/src/QuakeBoard.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import createEngine, {DagreEngine, DefaultNodeModel, DiagramModel} from '@projectstorm/react-diagrams';
-import {Item, Menu, Separator, Submenu, useContextMenu} from "react-contexify";
+import createEngine, {DagreEngine, DefaultNodeModel, DiagramEngine, DiagramModel} from '@projectstorm/react-diagrams';
+import {Item, Menu, useContextMenu} from "react-contexify";
 import {CanvasWidget} from "@projectstorm/react-canvas-core";
 import styled from "styled-components";
 import 'react-contexify/dist/ReactContexify.css';
 
-export type Props = {}
+export type Props = {
+  model: any,
+  onChange: (model: any) => any
+}
 
 const MENU_ID = 'blahblah';
 
@@ -27,24 +30,9 @@ function QuakeBoard(props: Props) {
   const model = React.useMemo(() => new DiagramModel(), []);
 
   const data = React.useMemo(() => {
-    let nodes = [];
-    let links: any = [];
-
-    const node1 = new DefaultNodeModel('Node 1', 'rgb(0,192,255)');
-    const port1 = node1.addOutPort('Out');
-    nodes.push(node1);
-
-    const node2 = new DefaultNodeModel('Node 2', 'rgb(192,255,0)');
-    const port2 = node2.addInPort('In');
-    nodes.push(node2);
-
-    const link1 = port1.link(port2);
-
-    links.push(link1);
-
     return {
-      nodes: nodes,
-      links: links
+      nodes: [],
+      links: []
     };
   }, []);
 
@@ -70,23 +58,85 @@ function QuakeBoard(props: Props) {
     })
   }
 
-  const handleItemClick = React.useCallback(
-    (props: any) => console.log(props), []
+  function addNode(event: MouseEvent, engine: DiagramEngine, type: string) {
+    let node: DefaultNodeModel;
+    switch (type) {
+      case 'out':
+        node = new DefaultNodeModel('Source', 'rgb(0,192,255)');
+        node.addOutPort('Out');
+        node.getOptions().extras = {"type": "out"};
+        break;
+      case 'in':
+        node = new DefaultNodeModel('Target', 'rgb(192,255,0)');
+        node.addInPort('In');
+        node.getOptions().extras = {"type": "in"};
+        break;
+      default:
+        node = new DefaultNodeModel('default', 'rgb(0,192,192)');
+        node.addInPort('In');
+        node.addOutPort('Out');
+        node.getOptions().extras = {"type": "inout"};
+        break;
+    }
+
+    const point = engine.getRelativeMousePoint(event);
+    node.setPosition(point);
+
+    return node;
+  }
+
+  const addSource = React.useCallback(
+    ({event}) => {
+      let node = addNode(event, engine, 'out');
+      model.addNode(node);
+
+      let serialize = model.serialize();
+      props.onChange(serialize);
+
+      engine.repaintCanvas();
+    }, [engine, model, props]
   );
 
+  const addFilter = React.useCallback(
+    (props: any) => {
+      console.log(props)
+    }, []
+  );
+
+  const addLambda = React.useCallback(
+    ({event}) => {
+      let node = addNode(event, engine, 'default');
+      model.addNode(node);
+
+      let serialize = model.serialize();
+      props.onChange(serialize);
+
+      engine.repaintCanvas();
+    }, [engine, model, props]
+  );
+
+  const addTarget = React.useCallback(
+    ({event}) => {
+      let node = addNode(event, engine, 'in');
+      model.addNode(node);
+
+      let serialize = model.serialize();
+      props.onChange(serialize);
+
+      engine.repaintCanvas();
+    }, [engine, model, props]
+  );
+
+
+
   return (
-    <StyledDiv onDoubleClick={handleContextMenu}>
-      <StyledCanvasWidget engine={engine} />
+    <StyledDiv onContextMenu={handleContextMenu}>
+      <StyledCanvasWidget engine={engine}/>
       <Menu id={MENU_ID}>
-        <Item onClick={handleItemClick}>Item 1</Item>
-        <Item onClick={handleItemClick}>Item 2</Item>
-        <Separator/>
-        <Item disabled>Disabled</Item>
-        <Separator/>
-        <Submenu label="Foobar">
-          <Item onClick={handleItemClick}>Sub Item 1</Item>
-          <Item onClick={handleItemClick}>Sub Item 2</Item>
-        </Submenu>
+        <Item onClick={addSource}>add Source</Item>
+        <Item onClick={addFilter}>add Filter rule</Item>
+        <Item onClick={addLambda}>add Lambda</Item>
+        <Item onClick={addTarget}>add Target</Item>
       </Menu>
     </StyledDiv>
   );
@@ -100,7 +150,11 @@ const StyledDiv = styled.div`
 `
 
 const StyledCanvasWidget = styled(CanvasWidget)`
-  background: #333333;
   width: 100%;
   height: 100%;
+
+  background-size: 50px 50px;
+  background-color: rgb(60, 60, 60);
+  background-image: linear-gradient(0deg, transparent 24%, rgba(255, 255, 255, 0.05) 25%, rgba(255, 255, 255, 0.05) 26%, transparent 27%, transparent 74%, rgba(255, 255, 255, 0.05) 75%, rgba(255, 255, 255, 0.05) 76%, transparent 77%, transparent), linear-gradient(90deg, transparent 24%, rgba(255, 255, 255, 0.05) 25%, rgba(255, 255, 255, 0.05) 26%, transparent 27%, transparent 74%, rgba(255, 255, 255, 0.05) 75%, rgba(255, 255, 255, 0.05) 76%, transparent 77%, transparent);
+
 `
