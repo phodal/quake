ff37773ce6e1c19854ce416a7fd9b7157dd99d23
Phodal Huang <h@phodal.com>
2022-01-19 20:12:06 +0800

    lint: fix lint

--- quake_core/src/entry/entry_by_path.rs
+++ quake_core/src/entry/entry_by_path.rs
@@ -33,7 +33,7 @@ pub fn entry_file_dump(
     if let Some(define) = defines.find(&*entry_type) {
         for (key, prop) in define.to_field_type() {
             if let MetaProperty::Date(_date) = prop {
-                let text = file.property(&key).unwrap_or("".to_string());
+                let text = file.property(&key).unwrap_or_else(|| "".to_string());
                 let value = quake_time::string_date_to_unix(&text);
                 file.update_property(&key, &value);
             }
