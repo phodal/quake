5c0735bbf0a68d1ae06418bc90b23e37f74d12d3
Elliot <hack00mind@gmail.com>
2022-02-21 17:26:50 +0800

    feat(GUI): init with dioxus

--- quake_gui/README.md
+++ quake_gui/README.md
@@ -1,2 +1,5 @@
 # Quake gui
 
+## Technical Spike
+
+- React integration: [dioxus-react-example](https://github.com/Eliot00/dioxus-react-example)
--- quake_gui/src/main.rs
+++ quake_gui/src/main.rs
@@ -1,1 +1,15 @@
-fn main() {}
+use dioxus::prelude::*;
+
+fn main() {
+    dioxus::desktop::launch(app);
+}
+
+fn app(cx: Scope) -> Element {
+    cx.render(rsx!(
+        link { rel: "stylesheet", href: "https://unpkg.com/tailwindcss@^2.0/dist/tailwind.min.css" },
+        h1 {
+            class: "text-2xl",
+            "Welcome"
+        }
+    ))
+}
